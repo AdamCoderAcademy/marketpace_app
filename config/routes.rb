5c61cee2044bfae946f5e5abea332975ee02e70f
Adam Hyde <adamjameshyde88@hotmail.com>
2020-03-06 14:05:08 +1000

    Added Stripe funtionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+  resources :products
+  devise_for :users
+  get 'welcome/index'
+  root 'welcome#index'
+  get "orders", to: "orders#new", as: "new_order"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
