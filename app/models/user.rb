26d86c70edadca80ba93e589aee18939864a0cd3
Adam Hyde <adamjameshyde88@hotmail.com>
2020-03-10 16:57:04 +1000

    remove drop table file from db files

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :products
-  has_many :reviews
-  
+  has_many :reviews, through: :products
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/migrate/20200305063957_remove_user_from_product.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserFromProduct < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :products, :user_id, :integer
-  end
-end
